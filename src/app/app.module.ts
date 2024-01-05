4bca92b9e8d2087526e4352381d61adc13ece585
NuevoBienestar <amorybienestar0@gmail.com>
2024-01-05 14:45:39 -0300

    pasaje de diseño de la pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { AcercadeComponent } from './acercade/acercade.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { PiepaginaComponent } from './piepagina/piepagina.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercadeComponent,
+    ServiciosComponent,
+    ContactoComponent,
+    PiepaginaComponent
   ],
   imports: [
     BrowserModule,
